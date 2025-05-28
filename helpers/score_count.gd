666318d51a480f971c08b3bcf69ed2cc144be3c7
sion <n.siomusic@gmail.com>
2025-05-28 09:49:46 +0700

    polishing: score increase now calls +100 animation

--- helpers/score_count.gd
+++ helpers/score_count.gd
@@ -1,7 +1,14 @@
 extends Label
 
 @onready var score_label = $"."
+@onready var animation = $AnimationPlayer
 
+func _ready() -> void:
+	State.score_add.connect(_score_add)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	score_label.text = " Score\n" + str(State.score)
+
+func _score_add():
+	animation.play("score_add")
